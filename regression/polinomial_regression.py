787ef9f2c0dd1759690608e4bdda1dcff64f094c
Tamara Katic <tamarakatic@gmail.com>
2018-03-08 17:14:04 +0100

    Try polinomial regression

---
+++ regression/polinomial_regression.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+
+dataset = pd.read_csv("Position_Salaries.csv")
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+linear_regression = LinearRegression()
+linear_regression.fit(X, y)
+
+poly_reg = PolynomialFeatures(degree=4)
+X_poly = poly_reg.fit_transform(X)
+linear_regression_2 = LinearRegression()
+linear_regression_2.fit(X_poly, y)
+
+plt.scatter(X, y, color='red')
+plt.plot(X, linear_regression.predict(X), color='blue')
+plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+plt.scatter(X, y, color='red')
+plt.plot(X, linear_regression_2.predict(poly_reg.fit_transform(X)), color='blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
