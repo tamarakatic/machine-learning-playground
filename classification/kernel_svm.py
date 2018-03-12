785ed853a19ce90118f44ff1ba116dd7e316b5af
Tamara Katic <tamarakatic@gmail.com>
2018-03-12 22:59:49 +0100

    Fit Kernel SVM

---
+++ classification/kernel_svm.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+from plot_and_pretty_print import pretty_print_cm
+from plot_and_pretty_print import plot_classifier
+
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+from sklearn.svm import SVC
+from sklearn import metrics
+
+
+dataset = pd.read_csv("Social_Network_Ads.csv")
+X = dataset.iloc[:, 2:4].values
+y = dataset.iloc[:, 4].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.fit_transform(X_test)
+
+classifier = SVC(kernel='rbf', random_state=0)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+print(y_pred)
+
+labels = ['Purchased', 'Not_Purchased']
+cm = confusion_matrix(y_test, y_pred)
+pretty_print_cm(cm, labels)
+
+f1_score = metrics.f1_score(y_test, y_pred)
+print("F1-score: {}".format(f1_score))
+
+plot_classifier(X_train, y_train, classifier)
+
+plot_classifier(X_test, y_test, classifier, False)
