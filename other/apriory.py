fef77a7123dcad511babb1aedb117683ee78bdd3
Tamara Katic <tamarakatic@gmail.com>
2018-03-26 00:11:29 +0200

    Try apriory algorithm

---
+++ other/apriory.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+from apyori import apriori
+
+
+dataset = pd.read_csv("Market_Basket_Optimisation.csv", header=None)
+transaction = []
+
+for i in range(0, 7501):
+    transaction.append([str(dataset.values[i, j]) for j in range(0, 20)])
+
+rules = apriori(transaction, min_support=0.003, min_confidence=0.2, min_lift=3, min_length=2)
+
+list_of_rules = list(rules)
