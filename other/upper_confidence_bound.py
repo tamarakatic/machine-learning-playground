3f70681395642326e8a90bcedaf93026e96af21d
Tamara Katic <tamarakatic@gmail.com>
2018-03-27 21:28:54 +0200

    Create ucb algorithm

---
+++ other/upper_confidence_bound.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+
+
+dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
+
+N = 10000
+d = 10
+numbers_of_selections = [0] * d
+sums_of_rewards = [0] * d
+ads_selected = []
+total_rewards = 0
+
+for n in range(0, N):
+    ad = 0
+    max_upper_bound = 0
+    for i in range(0, d):
+        if(numbers_of_selections[i] > 0):
+            avarage_reward = sums_of_rewards[i] / numbers_of_selections[i]
+            confidence_interval = math.sqrt(3/2 * math.log(n + 1) / numbers_of_selections[i])
+            upper_bound = avarage_reward + confidence_interval
+        else:
+            upper_bound = 1e400
+        if upper_bound > max_upper_bound:
+            max_upper_bound = upper_bound
+            ad = i
+    ads_selected.append(ad)
+    numbers_of_selections[ad] += 1
+    reward = dataset.values[n, ad]
+    sums_of_rewards[ad] += reward
+    total_rewards += reward
+
+plt.hist(ads_selected)
+plt.title('Histogram of ads selections')
+plt.xlabel('Ads')
+plt.ylabel('Number of times each ad was selected')
+plt.show()
