75237a51ac401e1a7fbc8f6a94cbde6b2e9d5ec5
Tamara Katic <tamarakatic@gmail.com>
2018-03-29 17:39:28 +0200

    Try nlp on restaurant reviews

---
+++ other/nlp_restaurant_reviews.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import re
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+from sklearn import metrics
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.naive_bayes import GaussianNB
+
+nltk.download('stopwords')
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t', quoting=3)
+corpus = []
+
+for i in range(0, dataset.shape[0]):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review
+              if word not in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+cv = CountVectorizer(max_features=1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:, 1].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size=0.20,
+                                                    random_state=0)
+
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+cm = confusion_matrix(y_test, y_pred)
+print("Confusion matrix: {}".format(cm))
+
+f1_score = metrics.f1_score(y_test, y_pred)
+print("F1-score: {}".format(f1_score))
