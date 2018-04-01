ba0ce987d9859662569cd281269457e8ca266834
Tamara Katic <tamarakatic@gmail.com>
2018-04-01 21:40:14 +0200

    Update ann script

--- deep_learning/ann.py
+++ deep_learning/ann.py
@@ -1,34 +1,14 @@
 import pandas as pd
-
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.metrics import confusion_matrix
+from data_preprocessing import preprocess
 
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
 
+from sklearn.metrics import confusion_matrix
+
 dataset = pd.read_csv("Churn_Modelling.csv")
-X = dataset.iloc[:, 3:13].values
-y = dataset.iloc[:, 13].values
-
-label_encoder_X_1 = LabelEncoder()
-X[:, 1] = label_encoder_X_1.fit_transform(X[:, 1])
-label_encoder_X_2 = LabelEncoder()
-X[:, 2] = label_encoder_X_2.fit_transform(X[:, 2])
-
-one_hot_encoder = OneHotEncoder(categorical_features=[1])
-X = one_hot_encoder.fit_transform(X).toarray()
-X = X[:, 1:]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.fit_transform(X_test)
-
+X_train, X_test, y_train, y_test = preprocess(dataset)
 
 classifier = Sequential()
 
